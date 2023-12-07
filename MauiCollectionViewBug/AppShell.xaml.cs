84ccee7fb4384b18ce2927a2743ea310f3c59831
Alberto Rodríguez <rodriguezo.alberto@gmail.com>
2023-12-06 18:19:21 -0600

    Add project files.

---
+++ MauiCollectionViewBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCollectionViewBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiCollectionViewBug/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiCollectionViewBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        public Monkey[] Monkeys { get; set; } =
+            [
+                new Monkey { Name = "Baboon", ImageUrl = "https://i.pinimg.com/originals/97/a9/92/97a9929f12ce116ec9a00449912cc3aa.jpg", Location = "Africa" },
+                new Monkey { Name = "Capuchin Monkey", ImageUrl = "https://i.pinimg.com/originals/97/a9/92/97a9929f12ce116ec9a00449912cc3aa.jpg", Location = "Africa" },
+                new Monkey { Name = "Blue Monkey", ImageUrl = "https://i.pinimg.com/originals/97/a9/92/97a9929f12ce116ec9a00449912cc3aa.jpg", Location = "Africa" },
+                new Monkey { Name = "Squirrel Monkey", ImageUrl = "https://i.pinimg.com/originals/97/a9/92/97a9929f12ce116ec9a00449912cc3aa.jpg", Location = "Africa" },
+                new Monkey { Name = "Golden Lion Tamarin", ImageUrl = "https://i.pinimg.com/originals/97/a9/92/97a9929f12ce116ec9a00449912cc3aa.jpg", Location = "Africa" },
+            ];
+    }
+
+    public class Monkey
+    {
+        public required string Name { get; set; }
+        public required string ImageUrl { get; set; }
+        public required string Location { get; set; }
+    }
+}
